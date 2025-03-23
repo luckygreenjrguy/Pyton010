a7d913b6c4aaefc7fb571ae08073c03efc79486e
luckygreenjrguy <luckygreenjr@gmail.com>
2025-03-23 15:21:08 -0400

    data_types_and_variables
    Name,age, Mad Libs game

---
+++ data_types_and_variables.py
@@ -0,0 +1,14 @@
+name="Jamal"
+age="28"
+print(name, " is ", age)
+
+
+print(f"My name is {name} and I am {age} years old.")
+name=input("enter a name")
+print(f"My name is {name} and I am {age} years old.")
+age=input("enter an age")
+print(f"My name is {name} and I am {age} years old.")
+#food=input("f{name} likes whay food: ")
+food = input(f"{name} likes what food: ")
+print(f"{name} likes {food}!")
+
